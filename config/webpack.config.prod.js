dc36f4486f99a6416f9e1af566d537ef5f0aff09
nwolf14 <norbert.wolff@intive.com>
2018-06-13 22:50:50 +0200

    init project

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,19 @@
+module.exports = {
+	devtool: 'cheap-module-source-map',
+	entry: './src/index.js',
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
+};
